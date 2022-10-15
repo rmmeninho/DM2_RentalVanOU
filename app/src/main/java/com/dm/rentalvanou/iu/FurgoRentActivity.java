b0daff93c838921f3b499952305956b0e3cc81f2
Roberbot <marcianorober@gmail.com>
2022-10-15 20:22:11 +0200

    Actualización de Fecha en FurgoRentActivity

--- app/src/main/java/com/dm/rentalvanou/iu/FurgoRentActivity.java
+++ app/src/main/java/com/dm/rentalvanou/iu/FurgoRentActivity.java
@@ -3,6 +3,8 @@ package com.dm.rentalvanou.iu;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
@@ -84,10 +86,23 @@ public class FurgoRentActivity extends AppCompatActivity {
 
         tvCoste.setText(String.valueOf(coste));
 
+       tvFecha_ini.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               abrirCalendario(tvFecha_ini);
+           }
+       });
+
+        tvFecha_fin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCalendario(tvFecha_fin);
+            }
+        });
 
     }
 
-    private void abrirCalendario() {
+    private void abrirCalendario(TextView v) {
         Calendar calendario = Calendar.getInstance();
         int anho = calendario.get(Calendar.YEAR);
         int mes = calendario.get(Calendar.MONTH);
@@ -96,10 +111,9 @@ public class FurgoRentActivity extends AppCompatActivity {
         DatePickerDialog aux = new DatePickerDialog(FurgoRentActivity.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                fecha = year + "/" + month + "/" + dayOfMonth;
+                v.setText( year + "/" + month + "/" + dayOfMonth);
             }
         }, anho, mes, dia);
-
         aux.show();
     }
 
